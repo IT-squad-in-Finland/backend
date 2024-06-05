7c56e447133973e394af99df663f3648a558069d
stevenzack <zigzigcheers@gmail.com>
2024-06-05 12:02:24 +0300

    Just a Backup

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import mysql from 'mysql';
 
 const app = express();
 const port = 8080;
 
+const dbc=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'forum'
+})
+dbc.connect(e=>{
+    if(e)throw e;
+    console.log('mysql connected');
+});
 app.get('/', (r, res) => {
+    dbc.query('select * from users',(e,vs)=>{
+        if(e){
+            console.log(e);
+            return;
+        }
+        console.log(`users: ${vs}`);
+    })
     res.send('ok')
 })
 
